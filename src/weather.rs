9d0303fdb9d5a6c4b52bbe1162238179bfb07add
haydenfish <hayden.fish@icloud.com>
2024-09-18 21:50:55 -0700

    Change visibility of weather related struct fields

--- src/weather.rs
+++ src/weather.rs
@@ -2,50 +2,50 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Weather {
-    location: Location,
-    current: Current,
-    forecast: Forecast,
+    pub location: Location,
+    pub current: Current,
+    pub forecast: Forecast,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Location {
-    name: String,
-    region: String,
-    country: String,
-    localtime: String,
+    pub name: String,
+    pub region: String,
+    pub country: String,
+    pub localtime: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Current {
-    temp_c: f32,
-    temp_f: f32,
-    is_day: u8,
-    condition: Condition,
+    pub temp_c: f32,
+    pub temp_f: f32,
+    pub is_day: u8,
+    pub condition: Condition,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Condition {
-    text: String,
-    code: u16,
+    pub text: String,
+    pub code: u16,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Forecast {
-    forecastday: Vec<Day>,
+    pub forecastday: Vec<Day>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Day {
-    hour: Vec<Hour>,
+    pub hour: Vec<Hour>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Hour {
-    time: String,
-    temp_c: f32,
-    temp_f: f32,
-    is_day: u8,
-    condition: Condition,
+    pub time: String,
+    pub temp_c: f32,
+    pub temp_f: f32,
+    pub is_day: u8,
+    pub condition: Condition,
 }
 
 impl Weather {
